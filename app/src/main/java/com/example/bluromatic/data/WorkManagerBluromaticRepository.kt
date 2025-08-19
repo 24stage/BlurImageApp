984aef7a4c4f3598191bff0d6d58078d902a779f
24stage <wqstage@163.com>
2025-08-19 18:12:04 +0800

    创建cleanupWorker和saveImageToFileWorker,实现工作链clean->blur->save

--- app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ app/src/main/java/com/example/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -19,6 +19,7 @@ package com.example.bluromatic.data
 import android.content.Context
 import android.net.Uri
 import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
@@ -26,6 +27,8 @@ import com.example.bluromatic.KEY_BLUR_LEVEL
 import com.example.bluromatic.KEY_IMAGE_URI
 import com.example.bluromatic.getImageUri
 import com.example.bluromatic.workers.BlurWorker
+import com.example.bluromatic.workers.CleanupWorker
+import com.example.bluromatic.workers.SaveImageToFileWorker
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 
@@ -37,12 +40,19 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
 
     // 实现接口 BluromaticRepository 的方法，用于发起“模糊图片”的后台任务。
     override fun applyBlur(blurLevel: Int) {
+        //调用 beginWith() 方法返回 WorkContinuation 对象，并为包含第一个工作请求的 WorkRequest 链创建起点。
+        var continuation = workManager.beginWith(OneTimeWorkRequest.from(CleanupWorker::class.java))
         //  创建一个只执行一次的 WorkRequest，指定任务是 BlurWorker
         val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
         // 通过 setInputData 方法，把图片的 Uri 和模糊等级一起传递给 BlurWorker
         blurBuilder.setInputData(createInputDataForWorkRequest(blurLevel,imageUri))
-        // 把这个任务加入 WorkManager 队列，系统会自动调度执行。
-        workManager.enqueue(blurBuilder.build())
+//        // 把这个任务加入 WorkManager 队列，系统会自动调度执行。
+//        workManager.enqueue(blurBuilder.build())
+        continuation = continuation.then(blurBuilder.build())
+        val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>()
+            .build()
+        continuation = continuation.then(save)
+        continuation.enqueue()
 
     }
 
